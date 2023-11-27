b1f642232002702b3b0d18ade5298ebcc92563ad
martinmulama <martin.mulama@yahoo.com>
2023-11-27 18:21:06 +0000

    added 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  const newArray = [];
+  for (const [idx, value] of array.entries()) {
+    newArray[idx] = appendString + value;
+  }
+
+  return newArray;
 }
